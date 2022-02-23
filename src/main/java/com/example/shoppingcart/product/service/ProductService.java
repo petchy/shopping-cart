d9d3ef454914dc7a2b29371b5b0592e0da775031
Akkarapol San <akkaraps@scg.com>
2022-02-23 23:55:13 +0700

    Create product service #1

---
+++ src/main/java/com/example/shoppingcart/product/service/ProductService.java
@@ -0,0 +1,20 @@
+package com.example.shoppingcart.product.service;
+
+
+import com.example.shoppingcart.product.model.Product;
+import com.example.shoppingcart.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+    public List<Product> searchProduct(String title) {
+        List <Product> result = productRepository.findProductsByTitle(title);
+        return result;
+    }
+}
